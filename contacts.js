f492c3c1b75e6edd51743d23940d71bff6fb2ab2
dima-zharyy <zharyydimka@gmail.com>
2022-09-13 21:41:20 +0300

    removed try/catch blocks

--- contacts.js
+++ contacts.js
@@ -8,92 +8,70 @@ const SHOW_TABLE = true;
 const HIDE_TABLE = false;
 
 const getContacts = async (view = SHOW_TABLE) => {
-  try {
-    const contacts = await fs.readFile(contactsPath, "utf-8");
-    const parsedContacts = JSON.parse(contacts);
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  const parsedContacts = JSON.parse(contacts);
 
-    if (view) {
-      console.table(parsedContacts);
-    }
-
-    return parsedContacts;
-  } catch (error) {
-    console.log(error.message);
+  if (view) {
+    console.table(parsedContacts);
   }
+
+  return parsedContacts;
 };
 
 const getContactById = async (contactId) => {
-  try {
-    const contacts = await getContacts(HIDE_TABLE);
-    const contact = contacts.find((item) => item.id === contactId);
+  const contacts = await getContacts(HIDE_TABLE);
+  const contact = contacts.find((item) => item.id === contactId);
 
-    if (!contact) {
-      console.log(
-        `\nSorry, there is no contact with id: ${contactId}. Check and try again!`
-          .red
-      );
-      return;
-    }
-
-    console.log(`\nHere is the contact you looked for:\n`.cyan);
-    console.table(contact);
-    return contact;
-  } catch (error) {
-    console.log(error.message);
+  if (!contact) {
+    console.log(
+      `\nSorry, there is no contact with id: ${contactId}. Check and try again!`
+        .red
+    );
+    return;
   }
+
+  console.log(`\nHere is the contact you looked for:\n`.cyan);
+  console.table(contact);
+  return contact;
 };
 
 const addContact = async (name, email, phone) => {
-  try {
-    const contacts = await getContacts(HIDE_TABLE);
-    const id = (Number(contacts[contacts.length - 1].id) + 1).toString();
-    const newContact = { id, name, email, phone };
-    const updatedContacts = [...contacts, newContact];
-    const jsonContacts = JSON.stringify(updatedContacts);
+  const contacts = await getContacts(HIDE_TABLE);
+  const id = (Number(contacts[contacts.length - 1].id) + 1).toString();
+  const newContact = { id, name, email, phone };
+  const updatedContacts = [...contacts, newContact];
+  const jsonContacts = JSON.stringify(updatedContacts);
 
-    try {
-      await fs.writeFile(contactsPath, jsonContacts);
-      await getContacts();
-      console.log(`\nSuccess! "${name}" has been added to Contacts!`.cyan);
-    } catch (error) {
-      console.log(error.message);
-    }
+  await fs.writeFile(contactsPath, jsonContacts);
+  await getContacts();
+  console.log(`\nSuccess! "${name}" has been added to Contacts!`.cyan);
 
-    return newContact;
-  } catch (error) {
-    console.log(error.message);
-  }
+  return newContact;
 };
 
 const removeContact = async (contactId) => {
-  try {
-    const contacts = await getContacts(HIDE_TABLE);
-    const contactToRemove = contacts.find((item) => item.id === contactId);
+  const contacts = await getContacts(HIDE_TABLE);
+  const contactToRemove = contacts.find((item) => item.id === contactId);
 
-    if (!contactToRemove) {
-      console.log(
-        `\nSorry, there is no contact with id: ${contactId}. Check and try again!`
-          .red
-      );
-      return;
-    }
-
-    const updatedContacts = contacts.filter((item) => item.id !== contactId);
-    const jsonContacts = JSON.stringify(updatedContacts);
-
-    try {
-      await fs.writeFile(contactsPath, jsonContacts);
-      await getContacts();
-      console.log(
-        `\nSuccess! Contact "${contactToRemove.name}" with id "${contactToRemove.id}" has been removed`
-          .cyan
-      );
-    } catch (error) {
-      console.log(error.message);
-    }
-  } catch (error) {
-    console.log(error.message);
+  if (!contactToRemove) {
+    console.log(
+      `\nSorry, there is no contact with id: ${contactId}. Check and try again!`
+        .red
+    );
+    return;
   }
+
+  const updatedContacts = contacts.filter((item) => item.id !== contactId);
+  const jsonContacts = JSON.stringify(updatedContacts);
+
+  await fs.writeFile(contactsPath, jsonContacts);
+  await getContacts();
+  console.log(
+    `\nSuccess! Contact "${contactToRemove.name}" with id "${contactToRemove.id}" has been removed`
+      .cyan
+  );
+
+  return contactToRemove ? contactToRemove : null;
 };
 
 module.exports = {
